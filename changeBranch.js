5522e094d66b99d30c38ec6f75e5569757d90f5c
elmanytas <elmanytas@gmail.com>
2019-02-20 08:12:28 +0100

    Change branch in the site file. Useful to build CI/CD .

---
+++ changeBranch.js
@@ -0,0 +1,25 @@
+//var repo = "git.paradigmadigital.com/santalucia/arquitectura/portal/indice.git"
+//var newBranches = "newbranches"
+repo = process.argv[2];
+newBranches = process.argv[3];
+
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+try {
+    const config = yaml.safeLoad(fs.readFileSync('/antora/site.yml', 'utf8'));
+
+    var arrayLength = config.content.sources.length;
+    for (var i = 0; i < arrayLength; i++) {
+        var re = new RegExp(repo);
+        if (re.test(config.content.sources[i].url)) {
+            config.content.sources[i].branches=newBranches
+        }
+    }
+
+    var outputConfig = yaml.safeDump(config)
+
+    fs.writeFileSync('/antora/site.yml', outputConfig);
+} catch (e) {
+    console.log(e);
+}
